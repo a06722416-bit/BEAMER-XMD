ff9240048a55b0bbb3527e2a81216f5b55776325
a06722416-bit <a06722416@gmail.com>
2026-05-12 21:20:44 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,4 @@
 module.exports = {
     ownerNumber: [''], // Put your number here e.g. ['2547XXXXXXXX']
-    sessionID: ''      // Paste your session ID here
+    sessionID: ''      // Paste your sesblinder~yipGRThI#eP7MAGaE58nU6VT99aooE4Y7kknl0_MGmKCOxRvJcbosion ID here
 };
